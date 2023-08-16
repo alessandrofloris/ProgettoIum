0c9ab12b35b714af6c6878ac0c9535ffd82cc3b0
efrau24 <e.frau24@studenti.unica.it>
2023-08-16 19:45:32 +0200

    Aggiunta Navbar e Activities
    Aggiunta navbar e implementato cambio alle activities corrispondenti

--- app/src/main/java/com/example/progetto/Search.java
+++ app/src/main/java/com/example/progetto/Search.java
@@ -1,49 +1,30 @@
 package com.example.progetto;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import androidx.annotation.NonNull;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.content.Intent;
-import android.view.MenuItem;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationView;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
-public class Home extends AppCompatActivity {
-
-    public DrawerLayout drawerLayout;
-    public ActionBarDrawerToggle actionBarDrawerToggle;
+public class Search extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-/*
-        drawerLayout = findViewById(R.id.drawer_layout1);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        setContentView(R.layout.activity_search);
 
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-
-        if (getSupportActionBar() != null)
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-*/
         FloatingActionButton home_button = findViewById(R.id.home_button);
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigationView);
 
         // Set Home selected
-        bottomNavigationView.setSelectedItemId(R.id.home_button);
+        bottomNavigationView.setSelectedItemId(R.id.search_button);
 
         // Perform item selected listener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -52,18 +33,16 @@ public class Home extends AppCompatActivity {
 
                 switch(item.getItemId())
                 {
+                    case R.id.home_button:
+                        return true;
                     case R.id.favorites_button:
                         startActivity(new Intent(getApplicationContext(),Favorites.class));
                         overridePendingTransition(0,0);
                         return true;
+                    case R.id.profile_button:
+                        return true;
                     case R.id.chat_button:
                         startActivity(new Intent(getApplicationContext(),Chat.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.home_button:
-                        return true;
-                    case R.id.profile_button:
-                        startActivity(new Intent(getApplicationContext(),Profile.class));
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.search_button:
@@ -82,19 +61,5 @@ public class Home extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),Home.class));
             }
         });
-
-
-
-
     }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
