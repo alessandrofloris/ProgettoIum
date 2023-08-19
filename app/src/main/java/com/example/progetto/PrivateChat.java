0c544e8c5d0ef4cfbaeef012946383185d5cf06a
efrau24 <e.frau24@studenti.unica.it>
2023-08-19 18:48:39 +0200

    Iniziato Layout Chat e Activity PrivateChat
    Creato layout preliminare della lista di utenti  e creata activity per la chat privata

--- app/src/main/java/com/example/progetto/PrivateChat.java
+++ app/src/main/java/com/example/progetto/PrivateChat.java
@@ -1,48 +1,22 @@
 package com.example.progetto;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class Chat extends AppCompatActivity {
+public class PrivateChat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
-
-
-        LinearLayout linearLayoutButton1 = findViewById(R.id.profileButton1);
-        linearLayoutButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Chat.this, PrivateChat.class));
-            }
-        });
-
-        LinearLayout linearLayoutButton2 = findViewById(R.id.profileButton2);
-        linearLayoutButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Chat.this, PrivateChat.class));
-            }
-        });
-
-        LinearLayout linearLayoutButton3 = findViewById(R.id.profileButton3);
-        linearLayoutButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Chat.this, PrivateChat.class));
-            }
-        });
+        setContentView(R.layout.activity_private_chat);
 
         FloatingActionButton home_button = findViewById(R.id.home_button);
 
@@ -50,7 +24,7 @@ public class Chat extends AppCompatActivity {
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigationView);
 
         // Set Home selected
-        bottomNavigationView.setSelectedItemId(R.id.chat_button);
+        bottomNavigationView.setSelectedItemId(R.id.profile_button);
 
         // Perform item selected listener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -65,10 +39,10 @@ public class Chat extends AppCompatActivity {
                         startActivity(new Intent(getApplicationContext(),Favorites.class));
                         overridePendingTransition(0,0);
                         return true;
-                    case R.id.chat_button:
-                        return true;
                     case R.id.profile_button:
-                        startActivity(new Intent(getApplicationContext(),Profile.class));
+                        return true;
+                    case R.id.chat_button:
+                        startActivity(new Intent(getApplicationContext(),Chat.class));
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.search_button:
