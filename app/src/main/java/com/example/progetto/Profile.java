4af966a928958e64fae62e9b95d33bd68c181822
efrau24 <e.frau24@studenti.unica.it>
2023-08-31 19:23:17 +0200

    Generi e regioni Profile activity
    Implementata la possibilità di aggiungere regioni e generi nel profilo personale

--- app/src/main/java/com/example/progetto/Profile.java
+++ app/src/main/java/com/example/progetto/Profile.java
@@ -1,10 +1,17 @@
 package com.example.progetto;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import android.content.Intent;
 import android.content.res.ColorStateList;
@@ -12,9 +19,11 @@ import android.graphics.Color;
 import android.graphics.Region;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -25,34 +34,31 @@ import java.util.List;
 
 public class Profile extends AppCompatActivity {
 
-    List<Genres> genresList = new ArrayList<>();
-    RecyclerView genresRecyclerView;
-    RecyclerView regionsRecyclerView;
-    List<Region> regionsList = new ArrayList<>();
-    GenreTagAdapter genreTagAdapter;
+
     ImageButton addGenres;
-    ImageButton addRegions;
+    ImageButton addLocations;
+
+    ArrayList<Locations> selectedLocations;
+    ArrayList<Genres> selectedGenres;
+    ChipGroup genres_chip_group_view;
+    ChipGroup locations_chip_group_view;
+
+
+    LinearLayout selected_genres_container_view;
+    LinearLayout selected_regions_container_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-
-
-        genresRecyclerView = findViewById(R.id.producer_genres_recyclerView);
         addGenres = findViewById(R.id.add_genres);
+        addLocations = findViewById(R.id.add_regions);
+        genres_chip_group_view = findViewById(R.id.genres_chip_group);
+        locations_chip_group_view = findViewById(R.id.regions_chip_group);
 
-        regionsRecyclerView = findViewById(R.id.producer_regions_recyclerView);
-        addRegions = findViewById(R.id.add_regions);
-
-
-        genreTagAdapter = new GenreTagAdapter(this, genresList);
-        genresRecyclerView.setHasFixedSize(true);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        genresRecyclerView.setLayoutManager(layoutManager);
-        genresRecyclerView.setAdapter(genreTagAdapter);
-
+        selected_genres_container_view = findViewById(R.id.genres_chip_layout);
+        selected_regions_container_view = findViewById(R.id.regions_chip_layout);
 
 
 
@@ -60,17 +66,17 @@ public class Profile extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Profile.this, SelectGenreActivity.class);
-                intent.putExtra("selected_genres_search", (Serializable) Profile.this.regionsList);
-                startActivity(intent);
+                intent.putExtra("selected_genres_search", (Serializable) Profile.this.selectedGenres);
+                startActivityIntentGenre.launch(intent);
             }
         });
 
-        addRegions.setOnClickListener(new View.OnClickListener() {
+        addLocations.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Profile.this, SelectRegionActivity.class);
-                intent.putExtra("selected_genres_search", (Serializable) Profile.this.regionsList);
-                startActivity(intent);
+                intent.putExtra("selected_locations_search", (Serializable) Profile.this.selectedLocations);
+                startActivityIntentLocation.launch(intent);
             }
         });
 
@@ -127,5 +133,100 @@ public class Profile extends AppCompatActivity {
         return stateList;
     }
 
+
+    public ActivityResultLauncher<Intent> startActivityIntentLocation = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                    Intent intent = result.getData();
+
+                    try{
+                        selectedLocations = (ArrayList<Locations>)intent.getSerializableExtra("selected_locations");
+                        updateLocations();
+                    } catch (Exception e) {
+                        Log.d("Get Extra From Intent", e.toString());
+                    }
+
+                }
+            });
+
+    public ActivityResultLauncher<Intent> startActivityIntentGenre = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                    Intent intent = result.getData();
+
+                    try{
+                        selectedGenres = (ArrayList<Genres>)intent.getSerializableExtra("selected_genres");
+                        updateGenres();
+                    } catch (Exception e) {
+                        Log.d("Get Extra From Intent", e.toString());
+                    }
+
+                }
+            });
+
+    private void updateLocations() {
+
+        locations_chip_group_view.removeAllViews();
+        Chip chip;
+
+        for(Locations location : this.selectedLocations) {
+            chip = new Chip(this);
+            chip.setId(ViewCompat.generateViewId());
+            chip.setText(location.getDesc());
+            chip.setCloseIconVisible(true);
+            chip.setOnCloseIconClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Chip chip = (Chip) view;
+                    locations_chip_group_view.removeView(chip);
+                    ArrayList<Locations> newSelectedLocations = new ArrayList<>();
+                    for(Locations location : selectedLocations) {
+                        if(!location.getDesc().equals(chip.getText())) {
+                            newSelectedLocations.add(location);
+                        }
+                    }
+                    selectedLocations = newSelectedLocations;
+                }
+            });
+            locations_chip_group_view.addView(chip);
+        }
+
+    }
+
+    private void updateGenres() {
+
+        genres_chip_group_view.removeAllViews();
+        Chip chip;
+        for(Genres genre : this.selectedGenres) {
+            chip = new Chip(this);
+            chip.setId(ViewCompat.generateViewId());
+            chip.setText(genre.getDesc());
+            chip.setCloseIconVisible(true);
+            chip.setOnCloseIconClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Chip chip = (Chip) view;
+                    genres_chip_group_view.removeView(chip);
+                    ArrayList<Genres> newSelectedGenres = new ArrayList<>();
+                    for(Genres genre : selectedGenres) {
+                        if(!genre.getDesc().equals(chip.getText())) {
+                            newSelectedGenres.add(genre);
+                        }
+                    }
+                    selectedGenres = newSelectedGenres;
+                }
+            });
+            genres_chip_group_view.addView(chip);
+        }
+
+
+
+    }
 
 }
